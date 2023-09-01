4032d16759937a6d317ca0ca25701f9973a36c54
naillegal <quluzade.nail@gmail.com>
2023-09-01 18:17:42 +0400

    django-ecommerce-multishop

--- multishop/settings.py
+++ multishop/settings.py
@@ -141,14 +141,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS=[
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL='media/'
-MEDIA_ROOT=BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 
